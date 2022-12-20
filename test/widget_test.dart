5d0abc493d939ca0366918aff3bd8d0175cd9498
Luis Antonio Zambrano Demera <zluis171997@gmail.com>
2022-12-20 14:06:12 -0500

    firebase Flutter
    chat utilizando firebase

---
+++ test/widget_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+//import 'package:gtk_flutter/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:firebaseflutter/main.dart';
+
+void main() {
+  testWidgets('Basic rendering', (tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const App());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('Firebase Meetup'), findsOneWidget);
+    expect(find.text('January 1st'), findsNothing);
+  });
+}
